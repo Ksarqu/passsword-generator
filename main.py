1b50f7e50e6d94e920e0e2a0e25ab6bb7bc7cd27
Ksarqu <ksarqu@gmail.com>
2021-09-09 21:27:49 +0200

    cosmetic changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from string import ascii_letters, digits
 from random import choice
 from os import path
 
+
 while True:
     try:
         passwd_len = int(input("how much characters do u want? "))
@@ -9,16 +10,20 @@ while True:
     except ValueError:
         print("Enter a number!")
 
+
 chars = list(ascii_letters) + list(digits)
 passwd = []
 for i in range(passwd_len):
     passwd.append(choice(chars))
+
+
 if not path.isfile("passwd.txt"):
     with open("passwd.txt", 'a') as f:
         f.write("".join(passwd))
 else:
     with open("passwd.txt", "w") as f:
         f.write("".join(passwd))
+
 print("".join(passwd))
 print("Saved to file also!")
 input()
